e875dc47e7de56cdee961d9d1728bc7819093512
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2017-09-13 18:16:42 +0200

    Add OSPA metric to score the crater detection

---
+++ workflow/scores.py
@@ -0,0 +1,32 @@
+from __future__ import division
+
+import itertools
+import numpy as np
+
+
+def score_craters(y_true, y_pred):
+    pass
+
+
+def score_iou(y_true, y_pred):
+    pass
+
+
+def score_completeness(y_true, y_pred):
+    pass
+
+
+def ospa(y_true, y_pred, p_norm, cut_off):
+    """
+
+    Parameters
+    ----------
+    y_true : list of tuples
+
+    y_pred : list of tuples
+
+    p_norm : int
+
+    cut_off : float
+    """
+    y_true
