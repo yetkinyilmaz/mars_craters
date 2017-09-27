59098accfc566f3a8090c2f33305aac72213bb88
Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
2017-09-27 18:01:09 +0200

    Replace wget by urlretrieve from urllib

--- download_data.py
+++ download_data.py
@@ -1,38 +1,14 @@
 import os
-from subprocess import check_call, CalledProcessError
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
 
 URLBASE = 'https://storage.ramp.studio/mars_craters/{}'
 DATA = [
     'images_quad_77.npy', ]
 LABELS = [
     'quad77_labels.csv', ]
-
-
-def download_file(url, output_file=None, shell=False):
-    """
-    Download a file from a url using `wget`
-
-    Parameters
-    ----------
-    url : str
-        Url to retrive the file
-    output_file : str, optional (default is None)
-        Local path to write the downloaded file
-    shell : bool, optional (default is False)
-        `subprocess.check_call` keyword
-
-    """
-    command_list = ['wget']
-
-    if output_file is not None:
-        command_list.append('-O')
-        command_list.append(output_file)
-
-    command_list.append(url)
-
-    print('\nRunning {}\n'.format(' '.join(command_list)))
-
-    check_call(command_list, shell=shell)
 
 
 def main(output_dir='data'):
@@ -42,23 +18,16 @@ def main(output_dir='data'):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    notfound = []
+    # notfound = []
     for url, filename in zip(urls, filenames):
         output_file = os.path.join(output_dir, filename)
 
         if os.path.exists(output_file):
             continue
 
-        try:
-            download_file(url, output_file)
-        except CalledProcessError:
-            notfound.append(filename)
-            os.remove(output_file)
-
-    if notfound:
-        print("The following file could not be downloaded:")
-        lis = ["\t{}\n".format(filename) for filename in notfound]
-        print("".join(lis))
+        print("Downloading from {}".format(url))
+        urlretrieve(url, filename=output_file)
+        print("File saved as {}".format(output_file))
 
 
 if __name__ == '__main__':
